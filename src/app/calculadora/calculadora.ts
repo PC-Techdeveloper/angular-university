44ad0088e502107e660580e108b11ae99fc92156
Jefferson Chavez Diaz <jefersonchavezdiaz.14@hotmail.com>
2026-01-07 13:06:25 -0500

    new settings on file formatted

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -8,14 +8,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './calculadora.css',
 })
 export class Calculadora {
-
-  title = 'App Calculadora'
+  title = 'App Calculadora';
 
   opA: number = 0;
   opB: number = 0;
   result: number = 0;
 
-  add(){
+  add() {
     this.result = this.opA + this.opB;
   }
 }
